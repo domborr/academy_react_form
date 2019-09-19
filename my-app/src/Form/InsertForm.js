7feaa5d4e18c1cb4a7197d3273aec5c9c35ff88a
Francesco Battista <francesco.battista@finconsgroup.local>
2019-09-19 15:45:42 +0200

    Final changes. Added JSON export

--- my-app/src/Form/InsertForm.js
+++ my-app/src/Form/InsertForm.js
@@ -8,16 +8,9 @@ import AppSettingsModel, { Banner, AppSettings } from "../Models/AppSettingsMode
 
 export default class InsertForm extends React.Component {
   state = {
-<<<<<<< Updated upstream
-    base_url:"",pages_path:"",layout_path:"",laucher_path:"",events_path:"",
-    epg:"",vod:"",linearAdv:"",videoAdv:"",
-    broadcastVideoElement:"",carouselId:"",streamEventObject:"",
-    lancher:"",events:"",
-=======
     base_url:"",pages_path:"",layout_path:"",events_path:"",laucher_path:"", events_path:"",
     epg:"",vod:"",linearAdv:"",videoADV:"",
     broadcastVideoElement:"",carouselID:"",streamEventObject:"",
->>>>>>> Stashed changes
     linearAdvEnabled:false,videoAdvEnabled:false,preroll:false,midroll:false,postroll:false,linearAdvApplication:"",
     cornerBannerCampaignId:"",cornerBannerStreamEvent:"",
     lsShapeBannerCampaignId:"",lsShapeBannerStreamEvent:"",
@@ -43,113 +36,15 @@ export default class InsertForm extends React.Component {
 
   onSubmit = event => {
     event.preventDefault();
-    let user = new User(this.state.name, this.state.lastname, this.state.age);
-    console.log(user);
-    this.service.exportToJson(JSON.stringify(user));
+    let appsettingsmodel = this.state;
+    console.log(appsettingsmodel);
+    this.service.exportToJson(JSON.stringify(appsettingsmodel));
   };
 
   render() {
     return (
       <Container>
         <Form onSubmit={this.onSubmit}>
-<<<<<<< Updated upstream
-          <FormGroup name="AppSettings"> 
-            <Label for="base_url">{t`base_url`}</Label>
-            <Input
-              required
-              onChange={this.onChange}
-              value={this.state.base_url}
-              type="text"
-              name="base_url"
-              id="base_url"
-              placeholder="Insert base url"
-            />
-            <Label for="pages_path">{t`Pages Path`}</Label>
-            <Input
-              required
-              onChange={this.onChange}
-              value={this.state.pages_path}
-              type="text"
-              name="pages_path"
-              id="pages_path"
-              placeholder="Insert Pages Path"
-            />
-            <Label for="layout_path">{t`Layout Path`}</Label>
-            <Input
-              required
-              onChange={this.onChange}
-              value={this.state.layout_path}
-              type="text"
-              name="layout_path"
-              id="layout_path"
-              placeholder="Insert Layout Path"
-            />
-            <Label for="laucher_path">{t`Laucher Path`}</Label>
-            <Input
-              required
-              onChange={this.onChange}
-              value={this.state.laucher_path}
-              type="text"
-              name="pages_path"
-              id="pages_path"
-              placeholder="Insert Laucher Path"
-            />
-            <Label for="events">{t`Events Path`}</Label>
-            <Input
-              required
-              onChange={this.onChange}
-              value={this.state.events_path}
-              type="text"
-              name="events_path"
-              id="events_path"
-              placeholder="Insert Events Path"
-            />
-            <FormGroup name = "platform">
-            <Label for="epg">{t`epg`}</Label>
-            <Input
-              required
-              onChange={this.onChange}
-              value={this.state.epg}
-              type="text"
-              name="epg"
-              id="epg"
-              placeholder="Insert epg"
-            />
-            <Label for="vod">{t`Vod`}</Label>
-            <Input
-              required
-              onChange={this.onChange}
-              value={this.state.vod}
-              type="text"
-              name="vod"
-              id="vod"
-              placeholder="Insert Vod"
-            />
-            <Label for="linearAdv">{t`linearAdv`}</Label>
-            <Input
-              required
-              onChange={this.onChange}
-              value={this.state.linearAdv}
-              type="text"
-              name="linearAdv"
-              id="linearAdv"
-              placeholder="Insert LinearAdv"
-            />
-            <Label for="videoAdv">{t`VideoAdv`}</Label>
-            <Input
-              required
-              onChange={this.onChange}
-              value={this.state.videoAdv}
-              type="text"
-              name="videoAdv"
-              id="videoAdv"
-              placeholder="Insert VideoAdv"
-            />
-
-            </FormGroup>
-           </FormGroup>
-          <Button>{t`Submit`}</Button>
-=======
           <FormGroup>
             <Label for="base_url">base_url</Label>
             <Input required onChange={this.onChange} value={this.state.base_url} type="text"
@@ -295,7 +190,7 @@ export default class InsertForm extends React.Component {
             </FormGroup>
 
           </FormGroup>
->>>>>>> Stashed changes
+          <Button> Submit </Button>
         </Form>
       </Container>
     );
