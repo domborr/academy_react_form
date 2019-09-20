ac4d4f28a3db95204ccb3ce00ef31f3d0b201f24
Frabat <battista.francesco3@gmail.com>
2019-09-20 13:13:16 +0200

    Form completed

--- my-app/src/Form/FormGenerator.js
+++ my-app/src/Form/FormGenerator.js
@@ -16,7 +16,37 @@ const schemaBase = {
     layout_path: {type: "string", title: "Layout_Path", default: "Layout Path"},
     events_path: {type: "string", title: "Events_Path", default: "Events Path"},
     laucher_path: {type: "string", title: "Laucher_Path", default: "Laucher Path"},
-    AdvertisingConfigurationSchema:{
+    platform:{
+      title:"Platform",
+      type:"object",
+      required:[],
+      properties:{
+        epg: {type: "string", title: "epg", default: "Epg"},
+        vod: {type: "string", title: "vod", default: "vod"},
+        linearADV: {type: "string", title: "linearADV", default: "linearADV"},
+        videoADV: {type: "string", title: "videoADV", default: "videoADV"},
+      }
+    },
+    hbbtv_configuration:{
+      title:"Hbbtv Configuration",
+      type: "object",
+      required:[],
+      properties:{
+        broadcastVideoElement: {type: "string", title: "broadcastVideoElement", default: "broadcastVideoElement"},
+        carouselID: {type: "number", title: "carouselID", default: "carouselID"},
+        streamEventObject: {type: "string", title: "streamEventObject", default: "streamEventObject"},
+      }
+    },
+    defaults:{
+      title: "Defaults",
+      type:"object",
+      required: [],
+      properties:{
+        lancher: {type: "string", title: "lancher", default: "lancher"},
+        events: {type: "string", title: "events", default: "events"}
+      }
+    },
+    advertisingConfigurationSchema:{
       title: "Advertising Configuration",
       type:"object",
       required:[],
@@ -25,7 +55,46 @@ const schemaBase = {
       videoAdvEnabled: {type: "boolean", title: "VideoAdvEnabled", default: false},
       preroll: {type: "boolean", title: "preroll", default: false},
       midroll: {type: "boolean", title: "midroll", default: false},
-      postroll: {type: "boolean", title: "postroll", default: false}
+      postroll: {type: "boolean", title: "postroll", default: false},
+      linearAdvApplication: {type: "string", title: "linearAdvApplication", default: "linearAdvApplication"},
+      linearAdvDemoConfig:{
+        title:"Linear Adv Demo Config",
+        type:"object",
+        required:[],
+        properties:{
+          cornerBanner:{
+            title:"Corner Banner",
+            type: "object",
+            required:[],
+            properties:{
+              campaignId: {type: "string", title: "campaignId", default: "campaignId"},
+              streamEvent: {type: "string", title: "streamEvent", default: "streamEvent"}
+            }
+
+          },
+          lsShapeBanner:{
+            title:"Ls Shape Banner",
+            type: "object",
+            required:[],
+            properties:{
+              campaignId: {type: "string", title: "campaignId", default: "campaignId"},
+              streamEvent: {type: "string", title: "streamEvent", default: "streamEvent"}
+            }
+
+          },
+          stripeBanner:{
+            title:"Stripe Banner",
+            type: "object",
+            required:[],
+            properties:{
+              campaignId: {type: "string", title: "campaignId", default: "campaignId"},
+              streamEvent: {type: "string", title: "streamEvent", default: "streamEvent"}
+            }
+
+          }
+
+        }
+      }
     }}
   }
     
@@ -49,6 +118,3 @@ return (
   )
 } 
 
-function Accazzo(){
-  console.log("Ciao");
-}
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import InsertForm from './Form/InsertForm';
 import './i18nInit';
 import FormGenerator from './Form/FormGenerator'
-ReactDOM.render(<FormGenerator />, document.getElementById('root'));
+ReactDOM.render(<InsertForm />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
